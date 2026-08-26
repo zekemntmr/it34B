d412679ca137547821c0e2aa3ed2b402d8b1968b
zekemntmr <zekemntmr@gmail.com>
2026-08-26 15:28:29 +0800

    updated readme.md

--- index.php
+++ index.php
@@ -1,33 +1,68 @@
 <?php
+require_once('config/config.php');
+
+$user_id = "root" ?? null;
+$user_email = "root" ?? null;
+
+$buttons = [
+    'Login',
+    'Logout',
+    'Create Record',
+    'Update Record',
+    'Delete Record',
+    'View Record',
+    'Upload File',
+    'Download',
+    'Search',
+    'Generate Report'
+];
+
+?>
+
+<table border="1" cellpadding="10">
+    <tr>
+        <th>Action</th>
+        <th>Test</th>
+    </tr>
+    <?php foreach ($buttons as $button): ?>
+        <tr>
+            <td><?= htmlspecialchars($button)?></td>
+            <td>
+                <form method="post">
+                    <input type="hidden" name="action"
+                        value="<?= htmlspecialchars($button) ?>"
+                    >
+                    <button type="submit">Test</button>
+                </form>    
+            </td>
+        </tr>
+    <?php endforeach; ?>
+</table>    
 
 
-require_once'config/config.php';
-require_once'includes/activity-logger.php';
-
+<?php
 if($_SERVER['REQUEST_METHOD'] === 'POST'){
-    $action = trim($_POST['action'] ?? '');
+    
+    $action = $_POST['action'] ?? "test_activity";
 
-    $user_id = $_SESSION['user_id'] ?? null;
-    $user_email = $_SESSION['user_email'] ?? null;
+    $status = random_int(0,1) === 1? 'success':'failed';
 
+    $success = logActivity(
+        $pdo,
+        $user_id,
+        $user_email,
+        $action,
+        $status
+    );
+
+    if($success){
+        echo "<p> Activity: " . htmlspecialchars($action) .
+            " Status: " . htmlspecialchars($status) . 
+            " Log Inserted Successfully </p>" ;
+    }else{
+        echo "<p>Failed to insert activity log</p>";
+    }
 }
 
 
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>I LOVE MORENA BADDIES</title>
-</head>
-<body>
-    <form method="POST">
-        <button
-            type="submit"
-            name="action"
-        >Pogi Button</button>
-    
-</body>
-</html>
